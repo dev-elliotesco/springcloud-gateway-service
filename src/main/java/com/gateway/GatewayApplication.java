a89e605a6ad0e78508e3e07f10ff0563b0858568
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-14 21:22:33 -0500

    add notification ms

--- src/main/java/com/gateway/GatewayApplication.java
+++ src/main/java/com/gateway/GatewayApplication.java
@@ -1,13 +1,19 @@
 package com.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(GatewayApplication.class, args);
+		logger.info("Welcome to Gateway service!");
 	}
 
 }
